2f5727f9c4e673c936f5fca34593979abbd02fe6
mamatha987 <mamathamm04@gmail.com>
2024-11-05 15:32:02 +0530

    Update avgGrade.py

--- avgGrade.py
+++ avgGrade.py
@@ -1,11 +1,1 @@
-# List of student grades
-grades = [85, 90, 78, 92, 88, 76, 95, 89]
-
-# Calculate the average grade
-average_grade = sum(grades) / len(grades)
-
-# Write the result to a text file
-with open("average_grade.txt", "w") as file:
-    file.write(f"The average grade is: {average_grade:.2f}")
-
-print("The average grade has been written to average_grade.txt")
+The average grade is: 86.63
